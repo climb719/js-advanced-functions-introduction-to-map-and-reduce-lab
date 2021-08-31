8b9b309a4d5ed47bf56abe80c453cfcfcfd9f7b7
climb719 <mary.oldweiler@gmail.com>
2021-08-31 11:15:23 -0400

    commit tree b268cf3421251d16322161c5b3893c5164daf4dc

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
-// Your code here
+function mapToNegativize(sourceArray) {
+   return sourceArray.map(e => e * -1);
+}
+
+function mapToNoChange(sourceArray) {
+    return sourceArray.map(e => e);
+}
+
+function mapToDouble(sourceArray) {
+    return sourceArray.map(e => e * 2);
+}
+
+function mapToSquare(sourceArray) {
+   return sourceArray.map(e => e * e );
+}
+
+function reduceToTotal(sourceArray, startingPoint) {
+    const reducer = (accumulator, currentValue) => accumulator + currentValue;
+    if (! startingPoint) {
+        return sourceArray.reduce(reducer)
+    }
+    else return startingPoint + sourceArray.reduce(reducer)
+}
+
+function reduceToAllTrue(sourceArray) {
+    for (let i = 0; i < sourceArray.length; i++) {
+        if (!sourceArray[i]) {
+            return false
+        }
+    }
+    return true 
+}
+
+function reduceToAnyTrue(sourceArray) {
+    for (let i = 0; i < sourceArray.length; i++) {
+        if (!!sourceArray[i]) {
+            return true
+        }
+    }
+    return false
+}
+
+
